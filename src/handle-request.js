93231d1f08058fe48e019fb83425d5d99d23174b
Wojciech Adaszyński <wa@colortv.com>
2016-11-10 10:37:05 +0100

    Enable cors

--- src/handle-request.js
+++ src/handle-request.js
@@ -2,6 +2,10 @@ const routes = require('./router')
 const finalhandler = require('finalhandler')
 
 function handleRequest(req, res){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   routes(req, res, finalhandler(req, res))
 }
 
